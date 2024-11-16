c5d8a9bb434bc68b8868a946c27a275b39d04a58
Jdonos0 <javier.donoso@alsea.cl>
2024-11-16 19:04:02 -0300

    fixes

--- src/app/dashboard/production/operations/manual-upload/page.jsx
+++ src/app/dashboard/production/operations/manual-upload/page.jsx
@@ -81,7 +81,7 @@ const ProductionManualHarvesting = () => {
             <div className="!z-5 relative flex flex-col rounded-[20px] bg-white bg-clip-border shadow-3xl shadow-shadow-500 dark:!bg-navy-800 dark:text-white dark:shadow-none w-full p-6">
               <CardTableManualHarvesting
                 data={dataManualHarvesting}
-                thead="Campo, Sector, Fecha cosecha, Especie, Variedad, Formato cosecha"    
+                thead="Campo, Sector, Fecha cosecha, Hora cosecha, Especie, Variedad, Formato cosecha"    
                 downloadBtn={true}
                 SearchInput={true}
                 actions={true}
